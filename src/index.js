c646662af0783ec0efb9436278b77f5f56ca1074
Thomas Baumann <tbaumann@mobcomp.ch>
2021-01-18 11:22:44 +0100

    adapted to react 1.7

--- src/index.js
+++ src/index.js
@@ -1,60 +1,65 @@
-import React from 'react';
-import { Chart } from 'taucharts';
-import equal from 'deep-equal';
+	'use strict';
 
-const TauChartReact = React.createClass({
-	displayName: 'TauChart',
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
 
-	propTypes: {
-		options: React.PropTypes.object.isRequired,
-		data: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
-	},
+	var _react = require('react');
 
-	componentWillUnmount() {
-		this.chart.destroy();
-	},
+	var _react2 = _interopRequireDefault(_react);
 
-	componentDidMount() {
-		this.renderChart();
-	},
+	var _taucharts = require('taucharts');
 
-	renderChart() {
-		this.chart = new Chart(
-			Object.assign({}, this.props.options, { data: this.props.data })
-		);
+	var _deepEqual = require('deep-equal');
 
-		if (this.props.height && this.props.width) {
-			this.chart.renderTo(this.refs.placeholder, {
-				height: this.props.height,
-				width: this.props.width
-			});
-		} else {
-			this.chart.renderTo(this.refs.placeholder);
+	var _deepEqual2 = _interopRequireDefault(_deepEqual);
+
+	var _props = require('prop-types');
+
+	var createReactClass = require('create-react-class');
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var TauChartReact = createReactClass({
+		displayName: 'TauChart',
+		
+		propTypes: {
+			options: _props.object.isRequired,
+			data: _props.arrayOf(_props.object).isRequired
+		},
+		
+		componentWillUnmount: function componentWillUnmount() {
+			this.chart.destroy();
+		},
+		componentDidMount: function componentDidMount() {
+			this.renderChart();
+		},
+		renderChart: function renderChart() {
+			this.chart = new _taucharts.Chart(Object.assign({}, this.props.options, { data: this.props.data }));
+
+			if (this.props.height && this.props.width) {
+				this.chart.renderTo(this.refs.placeholder, {
+					height: this.props.height,
+					width: this.props.width
+				});
+			} else {
+				this.chart.renderTo(this.refs.placeholder);
+			}
+		},
+		shouldComponentUpdate: function shouldComponentUpdate(newProps) {
+			if (newProps.className === this.props.className && newProps.height === this.props.height && newProps.width === this.props.width && (0, _deepEqual2.default)(newProps.options, this.props.options, { strict: true })) {
+				this.chart.setData(newProps.data);
+				return false;
+			}
+			return true;
+		},
+		componentDidUpdate: function componentDidUpdate() {
+			this.chart.destroy();
+			this.renderChart();
+		},
+		render: function render() {
+			return _react2.default.createElement('div', { style: { height: '100%' }, className: this.props.className, ref: 'placeholder' });
 		}
+	});
 
-	},
-
-	shouldComponentUpdate(newProps) {
-		if (newProps.className === this.props.className &&
-			newProps.height === this.props.height &&
-			newProps.width === this.props.width &&
-			equal(newProps.options, this.props.options, { strict: true })
-		) {
-			this.chart.setData(newProps.data);
-			return false;
-		}
-		return true;
-	},
-
-	componentDidUpdate() {
-		this.chart.destroy();
-		this.renderChart();
-	},
-
-	render() {
-		return (<div style={{height: '100%'}} className={this.props.className} ref="placeholder"></div>)
-	}
-
-});
-
-export default TauChartReact;
+	exports.default = TauChartReact;
